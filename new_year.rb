0d6389e83883a5c05d9005384f0028aacc2232ab
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-03 22:25:55 -0500

    Function returns max value in array

--- new_year.rb
+++ new_year.rb
@@ -3,12 +3,12 @@ p "New Year Chaos!"
 def min_bribes q
 	counter = 0
 
-	 for i in (q.size - 1).downto(0) 
-	 	if q[i] - (i + 1) >= 3
+	for i in (q.size - 1).downto(0) 
+		if q[i] - (i + 1) >= 3
 	 		return "To Chaotic!"
 	 	end	
-	 end	
-
+	end	
+	q.max
 end
 
-p min_bribes [5, 1, 2, 3, 7, 8, 6, 4]
+p min_bribes [1, 2, 3, 4, 6, 5, 7]
