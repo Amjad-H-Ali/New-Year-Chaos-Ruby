d7d3546330195fc93dfd8f76a26236f2ba3a0eab
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-03 22:34:05 -0500

    For every number in array, function checks if element before it is greater. If it is, it increments counter. Counter is returned at the end

--- new_year.rb
+++ new_year.rb
@@ -6,9 +6,21 @@ def min_bribes q
 	for i in (q.size - 1).downto(0) 
 		if q[i] - (i + 1) >= 3
 	 		return "To Chaotic!"
+	 	end
+
+	 	for j in [0, q[i] - 2].max ... i
+	 		if q[j] > q[i]
+	 			counter += 1
+	 		end	
 	 	end	
 	end	
-	q.max
+	counter
 end
 
-p min_bribes [1, 2, 3, 4, 6, 5, 7]
+p min_bribes [2, 5, 1, 3, 4]
+
+p min_bribes [1, 2, 5, 3, 7, 8, 6, 4]
+
+p min_bribes [5, 1, 2, 3, 7, 8, 6, 4]
+
+p min_bribes [1, 2, 5, 3, 4, 7, 8, 6]
