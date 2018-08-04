c9d1d864e69ff29af724a1fe0b1ea34435d9262b
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-08-03 22:11:32 -0500

    Function iterates over array backwards

--- new_year.rb
+++ new_year.rb
@@ -1,7 +1,12 @@
 p "New Year Chaos!"
 
 def min_bribes q
-	q
+	counter = 0
+
+	 for i in (q.size - 1).downto(0) 
+	 	p q[i]
+	 end	
+
 end
 
-p min_bribes [1, 2, 3]
+min_bribes [1, 2, 3, 4, 5, 6, 7]
